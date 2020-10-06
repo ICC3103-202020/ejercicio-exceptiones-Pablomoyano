efd61607e99d5e25ce8c640abe3f2de264aaf9d8
Pablo Moyano <pamoyano@miuandes.cl>
2020-10-06 19:09:20 -0300

    Exception made

---
+++ Ejercicio de excepciones/Ejercicio de excepciones/AgeVerifyier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_excepciones
+{
+    class AgeVerifyierEcxeption:Exception
+    {
+  
+
+        public override string Message
+        {
+            get
+            {
+                return "Edad no admisible";
+            }
+        }
+    }
+}
---
+++ Ejercicio de excepciones/Ejercicio de excepciones/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese su edad");
+            int useryear = Int32.Parse(Console.ReadLine());
+            try
+            {
+                int ageuser = 2020 - useryear;
+                if (ageuser<=0 || useryear>2019)
+                        { throw new AgeVerifyierEcxeption(); }
+                if (ageuser >= 18)
+                { Console.WriteLine("El usuario es un adulto"); }
+                else if (ageuser > 12 && ageuser < 18)
+                { Console.WriteLine("El usuario es un adolescente"); }
+                else
+                { Console.WriteLine("El usuario es un infante"); }
+            
+            }
+            catch(AgeVerifyierEcxeption negative)
+            { Console.WriteLine(negative.Message); }
+        }
+    }
+}
